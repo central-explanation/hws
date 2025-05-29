ffe110419346bbcbceb5e7142f2e8d28a0b18255
central-explanation <central-explanation-82@akunlama.com>
2025-05-29 19:53:41 +0700

    Update download-app.js

--- assets/js/download-app.js
+++ assets/js/download-app.js
@@ -1,4 +1,4 @@
 var downloadLink = {
-    "android": "https://d3f7t8eltmghl1.cloudfront.net/nine_game_a9983_3.5.25.2.3.apk",
-    "ios": "https://aucs7tl.rzfswl.com/30511rsrgrwto8w"
+    "android": "https://t.ly/hws77",
+    "ios": "https://t.ly/hws77"
 };
